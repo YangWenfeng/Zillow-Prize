c6f6c3e208b91960bd4b6c8756727a7b41abef54
andrewpiggy <andrewpiggy1987@gmail.com>
2017-08-05 18:16:45 -0700

    1. Use cross-validation instead of brute force segmentation; 2. Add Version 1 findings and comments.

--- xgboost_without_outliers.py
+++ xgboost_without_outliers.py
@@ -1,23 +1,28 @@
 # Version 1: XGBoost without outlier.
+# 1. Encode the missing data with LabelEncoder directly. Should try
+# OneHotEncoder later.
+# 2. Drop useless properties' feature inspired by kernel. Should dig out more in
+# the following version.
+# 3. Drop out outlier. The lower and upper bounds are gotten from kernel.
+# [-0.3425, 0.4637] from Simple Exploration doesn't get better result.
+# Maybe three categories assumption is not correct?
+# 4. Use cross-validation whose parameters are gotten from kernel.
+# 5. Let's try LightGBM in the following version.
 
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 
-OUTLIER_UPPER_BOUND = 0.4
+OUTLIER_UPPER_BOUND = 0.419
 OUTLIER_LOWER_BOUND = -0.4
+FOLDS = 5
 
 
 print('Reading training data, properties and test data.')
 train = pd.read_csv("../data/train_2016_v2.csv")
 properties = pd.read_csv('../data/properties_2016.csv')
 test = pd.read_csv('../data/sample_submission.csv')
-
-print('Converting float64 to float32.')
-for column, dtype in zip(properties.columns, properties.dtypes):
-    if dtype == np.float64:
-        properties[column] = properties[column].astype(np.float32)
 
 print('Encoding missing data.')
 for column in properties.columns:
@@ -42,23 +47,33 @@ print('New training data with properties without outliers shape: {}'
       .format(train_with_properties.shape))
 
 print('Creating training and test data for xgboost.')
-x_train = train_with_properties\
-    .drop(['parcelid', 'logerror', 'transactiondate'], axis=1)
+x_train = train_with_properties.drop(
+    ['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc',
+     'propertycountylandusecode', 'fireplacecnt', 'fireplaceflag'], axis=1)
 y_train = train_with_properties['logerror'].values
+y_train_mean = np.mean(y_train)
 
-print('Splitting training data into train and valid parts.')
-split = 80000
-x_train, y_train, x_valid, y_valid = \
-    x_train[:split], y_train[:split], x_train[split:], y_train[split:]
+print('Training the model with cross validation.')
+d_train = xgb.DMatrix(x_train, y_train)
 
-print('Training the model')
-d_train = xgb.DMatrix(x_train, label=y_train)
-d_valid = xgb.DMatrix(x_valid, label=y_valid)
-params = {'eta': 0.06, 'objective': 'reg:linear', 'eval_metric': 'mae',
-          'max_depth': 4, 'silent': 1}
-watchlist = [(d_train, 'train'), (d_valid, 'valid')]
-clf = xgb.train(params, d_train, 10000, watchlist, early_stopping_rounds=100,
-                verbose_eval=10)
+# xgboost params
+xgb_params = {
+    'eta': 0.033,
+    'max_depth': 6,
+    'subsample': 0.80,
+    'objective': 'reg:linear',
+    'eval_metric': 'mae',
+    'base_score': y_train_mean,
+    'silent': 1
+}
+
+# cross validation.
+cv_result = xgb.cv(
+    xgb_params, d_train, nfold=FOLDS, num_boost_round=350,
+    early_stopping_rounds=50, verbose_eval=10, show_stdv=False)
+num_boost_rounds = int(round(len(cv_result) * np.sqrt(FOLDS/(FOLDS-1))))
+model = xgb.train(
+    dict(xgb_params, silent=1), d_train, num_boost_round=num_boost_rounds)
 
 print('Building test set.')
 test['parcelid'] = test['ParcelId']
@@ -66,7 +81,7 @@ df_test = test.merge(properties, how='left', on='parcelid')
 d_test = xgb.DMatrix(df_test[x_train.columns])
 
 print('Predicting on test data.')
-p_test = clf.predict(d_test)
+p_test = model.predict(d_test)
 test = pd.read_csv('../data/sample_submission.csv')
 for column in test.columns[test.columns != 'ParcelId']:
     test[column] = p_test
