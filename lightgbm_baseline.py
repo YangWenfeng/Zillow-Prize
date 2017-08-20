d044758b4f321f2618de96dd70a65754ed5ab559
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-08-20 10:16:16 +0800

    add without outlier / dbscan performance

--- lightgbm_baseline.py
+++ lightgbm_baseline.py
@@ -5,18 +5,41 @@
 # 4. Predict the test data.
 # 5. Output to file.
 #
+# baseline
 # Training result: [678] d_train's l1: 0.067627	d_valid's l1: 0.0644382
 # Public score: 0.0646997
+# baseline + without outlier
+# Training result: [1276] d_train's l1: 0.0517348 d_valid's l1: 0.0525761
+# Public score: 0.0646017
+# baseline + without outlier + DBSCAN epsilon = 0.05, Number of clusters: 79234
+# Training result: [1280] d_train's l1: 0.0517544 d_valid's l1: 0.0525683
+# Public score: ?
+# baseline + without outlier + set parameter 'categorical_feature' in train
+# Training result: [441] d_train's l1: 0.0517809 d_valid's l1: 0.0528008
+
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
 from sklearn.preprocessing import LabelEncoder
 
+# OUTLIER_UPPER_BOUND = 0.419
+# OUTLIER_LOWER_BOUND = -0.4
+# FOLDS = 5
 
 print('Reading training data, properties and test data.')
 train = pd.read_csv("../data/train_2016_v2.csv")
 properties = pd.read_csv('../data/properties_2016.csv')
 test = pd.read_csv('../data/sample_submission.csv')
+# properties_latlng_cluster = pd.read_csv('../data/properties_latlng_cluster.csv')
+# properties = properties.merge(properties_latlng_cluster, how='left', on='parcelid')
+# properties = properties.drop(['cluster_latitude', 'cluster_longitude', 'longitude', 'latitude'], axis=1)
+
+# before object features encoding
+# categorical_feature = list(properties.columns[properties.dtypes == object].values)
+# categorical_feature.extend(['airconditioningtypeid', 'architecturalstyletypeid', 'buildingclasstypeid',
+#                             'heatingorsystemtypeid', 'storytypeid', 'regionidcity', 'regionidcounty',
+#                             'regionidneighborhood', 'regionidzip'])
+# categorical_feature = list(set(categorical_feature))
 
 print('Encoding missing data.')
 for column in properties.columns:
@@ -29,6 +52,14 @@ for column in properties.columns:
 
 print('Creating training and validation data for xgboost.')
 train_with_properties = train.merge(properties, how='left', on='parcelid')
+
+# print('Dropping out outliers.')
+# train_with_properties = train_with_properties[
+#     train_with_properties.logerror > OUTLIER_LOWER_BOUND]
+# train_with_properties = train_with_properties[
+#     train_with_properties.logerror < OUTLIER_UPPER_BOUND]
+# print('New training data with properties without outliers shape: {}'
+#       .format(train_with_properties.shape))
 
 train_index = []
 valid_index = []
@@ -50,6 +81,10 @@ y_valid = valid_dataset['logerror'].values
 d_train = lgb.Dataset(x_train, label=y_train)
 d_valid = lgb.Dataset(x_valid, label=y_valid)
 
+# x_train = train_with_properties.drop(['parcelid', 'logerror', 'transactiondate'], axis=1)
+# y_train = train_with_properties['logerror'].values
+# d_train = lgb.Dataset(x_train, label=y_train)
+
 print('Training the model.')
 # lightgbm params
 params = dict()
@@ -65,6 +100,18 @@ params['num_leaves'] = 512         # num_leaf
 params['min_data'] = 500           # min_data_in_leaf
 params['min_hessian'] = 0.05       # min_sum_hessian_in_leaf
 params['verbose'] = 0
+
+# cross valication
+# https://www.kaggle.com/aharless/lightgbm-outliers-remaining-cv
+# print("Cross-validating LightGBM model ...")
+# cv_results = lgb.cv(params, d_train, num_boost_round=10000, early_stopping_rounds=100,
+#                     verbose_eval=20, nfold=FOLDS)
+# print('Current parameters:\n', params)
+# print('\nBest num_boost_round:', len(cv_results['l1-mean']))
+# print('Best CV score:', cv_results['l1-mean'][-1])
+# num_boost_round = len(cv_results['l1-mean'])
+# # categorical_feature=categorical_feature
+# model = lgb.train(params, d_train, num_boost_round=num_boost_round, verbose_eval=10)
 
 model = lgb.train(params, d_train, num_boost_round=1000, valid_sets=[d_train, d_valid],
                   valid_names=['d_train', 'd_valid'], early_stopping_rounds=100,
